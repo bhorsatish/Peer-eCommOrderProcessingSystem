3a7bf261dbff63ea0f232f89f1685ad448076c23
Satish <satishbhor007@gmail.com>
2026-07-21 15:30:06 +0530

    develop : Added base configuration for the backend functionality

---
+++ src/peer_ecomm_ms/peer_ecomm_ms/Program.cs
@@ -0,0 +1,42 @@
+using eComm_ms.DBA;
+using eComm_ms.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll",
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
+builder.Services.AddDbContext<ECommDbContext>(options =>
+    options.UseSqlite("Data Source=\"H:\\eCommOrderProcessing\\src\\peer_database\\eCommDB.db\""));
+builder.Services.AddControllers();
+builder.Services.AddScoped<OrderDetailsService>();
+builder.Services.AddOpenApi();
+
+// Register the background service
+builder.Services.AddHostedService<OrderStatusUpdateService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "eComm_ms API v1");
+        options.RoutePrefix = "swagger";
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseCors("AllowAll");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();

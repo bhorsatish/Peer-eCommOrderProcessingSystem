7d674085da87e8a5929d2b833650215baf65f920
Satish <satishbhor007@gmail.com>
2026-07-21 15:39:42 +0530

    develop : Implemented the ecom db context along with the models

---
+++ src/peer_ecomm_ms/peer_ecomm_ms/DBA/ECommDbContext.cs
@@ -0,0 +1,57 @@
+﻿using eComm_ms.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eComm_ms.DBA
+{
+    public class ECommDbContext : DbContext
+    {
+        public ECommDbContext(DbContextOptions<ECommDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Products> Products { get; set; }
+
+        public DbSet<Users> Users { get; set; }
+
+        public DbSet<StatusStates> StatusStates { get; set; }
+
+        public DbSet<Orders> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired();
+                entity.Property(e => e.Price).IsRequired().HasPrecision(18, 2);
+                entity.Property(e => e.Icon).IsRequired();
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.UserId).IsRequired();
+                entity.Property(e => e.RoleId).IsRequired();
+                entity.Property(e => e.Password).IsRequired();
+            });
+
+            modelBuilder.Entity<StatusStates>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired();
+                entity.Property(e => e.Description).IsRequired();
+                entity.Property(e => e.Icon).IsRequired();
+            });
+
+            modelBuilder.Entity<Orders>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.UserId).IsRequired();
+                entity.Property(e => e.ProductId).IsRequired();
+                entity.Property(e => e.StatusId).IsRequired();
+            });
+        }
+    }
+}
